8bb7d86179805c81cc12e0b7a2109cd57b083067
Manjeet Pandey <61266636+Manjeet-pandey@users.noreply.github.com>
2024-12-14 15:32:36 +0545

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Unix Portfolio',
-  description: 'Software Engineer Portfolio with Unix Theme',
+  title: 'Manjeet Pandey',
+  description: 'Manjeet Pandey Portfolio',
 }
 
 export default function RootLayout({
