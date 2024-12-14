b85fc27e10729bb0e2b05224333a15d16ee5c193
Manjeet Pandey <61266636+Manjeet-pandey@users.noreply.github.com>
2024-12-14 15:27:37 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  basePath: "/portfolio", // <-- replace with your repo name.
 };
 
 module.exports = nextConfig;
